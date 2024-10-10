97596d878e0df0e2712b4163c9cfe9990286a11e
zhicheng29 <1783176286@qq.com>
2024-10-10 10:29:35 +0800

    feat: 🚀 flow menu

--- apps/work-flow/src/layout/Sider/index.tsx
+++ apps/work-flow/src/layout/Sider/index.tsx
@@ -1,12 +1,13 @@
-import { Avatar } from "antd";
-import { UserOutlined } from "@ant-design/icons";
+import { Avatar, Button, Divider } from "antd";
+import { PlusCircleOutlined, UserOutlined } from "@ant-design/icons";
 
 const SiderContainer: React.FC = () => {
   return (
     <div className="pl-2 py-2 h-ful">
       <div className="h-full  px-1.5 py-4 border rounded-xl bg-white flex flex-col items-center">
         <Avatar shape="square" size="large" icon={<UserOutlined />} />
-        SiderContainer
+        <Button className="mt-4" type="text" size="large" icon={<PlusCircleOutlined />} />
+        <Divider className="my-4" />
       </div>
     </div>
   );
